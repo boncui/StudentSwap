3ed5522ebf6869f4aff31d4f7c914f5b62302e28
David Cui <112504777+boncui@users.noreply.github.com>
2025-01-06 09:50:05 -0800

    server is running:)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from 'express'; //express is used to create the server
 import dotenv from 'dotenv'; //dotenv is used for managing enviorment variables
+import connectDB from './config/db';
+
 
 dotenv.config();
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5001; //PORT 5000 is being used by Mac
