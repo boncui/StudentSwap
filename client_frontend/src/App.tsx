8df7664f8c386bf0d347c361def35eac06317262
David Cui <112504777+boncui@users.noreply.github.com>
2025-01-21 13:52:28 -0600

    frontend operational

--- client_frontend/src/App.tsx
+++ client_frontend/src/App.tsx
@@ -1,19 +1,28 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import HousingContracts from './pages/HousingContracts';
+// import Dashboard from './pages/Dashboard';
+// import HousingContracts from './pages/HousingContracts';
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ textAlign: 'center', marginTop: '20%' }}>
+              <h1>FRONT END IS WORKING AND RUNNING</h1>
+            </div>
+          }
+        />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<div>Dashboard Page</div>} />
       </Routes>
     </Router>
-  )
-}
-
+  );
+};
 
 export default App;
