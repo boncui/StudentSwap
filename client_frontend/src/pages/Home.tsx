c8c797b1f3685258df4de316877609dfeec21327
David Cui <112504777+boncui@users.noreply.github.com>
2025-03-21 15:14:22 -0500

    removed unecessary shit

--- client_frontend/src/pages/Home.tsx
+++ client_frontend/src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Pricing from "../components/Pricing";
-import CTA from "../components/CTA";
 import Footer from "../components/Footer";
 
 const Home: React.FC = () => {
@@ -10,11 +9,8 @@ const Home: React.FC = () => {
     <div className="flex flex-col min-h-screen">
       
       <main className="flex-grow">
-        
         <Hero />
         <Features />
-        <Pricing />
-        <CTA />
       </main>
       <Footer />
     </div>
